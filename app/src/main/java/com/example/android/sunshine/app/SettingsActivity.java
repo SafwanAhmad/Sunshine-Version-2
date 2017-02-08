4ecb41cb18e252c14a2c7d60ca0a3d554e9cd0d7
Safwan Ahmad <safwan.pico@gmail.com>
2017-02-08 16:05:50 +0530

    Added shared preference change listener to detect changes in preferences when they are witten to persistent memory. While the local change is detect with preference change listerner. Hence we only update the summary based on the type of preference inside preference change listener. And taking actualactions when the preference value is updated in the persistent memory.

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -17,12 +17,16 @@ package com.example.android.sunshine.app;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+
+import com.example.android.sunshine.app.data.WeatherContract;
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings.
@@ -33,7 +37,8 @@ import android.preference.PreferenceManager;
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class SettingsActivity extends PreferenceActivity
-        implements Preference.OnPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener
+{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,15 +69,71 @@ public class SettingsActivity extends PreferenceActivity
                         .getString(preference.getKey(), ""));
     }
 
+    // We want to be notified about the change in shared preferences as long as we are inside
+    // the SettingActivity. So register and unregister accordingly.
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    // Override this method so that we can get notifications when the value of location status
+    // gets changed. The location status value corresponds to status of server.
+    // This method gets called after the preference is changed, which is important because we
+    // start our synchronization here.
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // We entered a new location
+        if (key.equals(getString(R.string.pref_location_key))) {
+            //Reset the value of location status (server status) to unknown
+            Utility.resetLocationStatus(this);
+
+            //Also launch service to download data, this will also update the location
+            //status preference. So we expect a future call with key = key_location_status
+            SunshineSyncAdapter.syncImmediately(this);
+        }
+
+        // If unit value changes, we need to update weather accordingly.
+        else if (key.equals(getString(R.string.pref_units_key))) {
+            // This will force the attached loader to perform load, which will tell the
+            // attached adapter to update views.
+            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
+        }
+
+        // If value of location status is changed, we perform update to summary as well as
+        // bind this preference so that in future if the value gets changed it is reflected
+        // inside setting activity also.
+        else if (key.equals(getString(R.string.key_location_status))) {
+            Preference locationStatus = findPreference(getString(R.string.pref_location_key));
+            bindPreferenceSummaryToValue(locationStatus);
+        }
+    }
+
+    // This is called before the state of the Preference is about to be updated and before the
+    // state is persisted.
     @Override
     public boolean onPreferenceChange(Preference preference, Object value) {
+        // We will only change the summary here. As preference is still not written to persistent
+        // memory.
+        setPreferenceSummary(preference, value);
+        return true;
+    }
+
+    //Helper method to set summary for different scenarios
+    private void setPreferenceSummary(Preference preference, Object value) {
+        // Take out the key and string representation of value
+        String key = preference.getKey();
         String stringValue = value.toString();
 
-        //Check if the update is for location preference, then reset the location status
-        //(server status) to unknown.
-        if (preference.equals(findPreference(getString(R.string.pref_location_key)))) {
-            Utility.resetLocationStatus(preference.getContext());
-        }
+        // Check if the change is for the list preference
         if (preference instanceof ListPreference) {
             // For list preferences, look up the correct display value in
             // the preference's 'entries' list (since they have separate labels/values).
@@ -81,11 +142,28 @@ public class SettingsActivity extends PreferenceActivity
             if (prefIndex >= 0) {
                 preference.setSummary(listPreference.getEntries()[prefIndex]);
             }
-        } else {
-            // For other preferences, set the summary to the value's simple string representation.
+        } else if (key.equals(getString(R.string.pref_location_key))) {
+            @SunshineSyncAdapter.LocationStatus int statusCode = Utility.getLocationStatus(this);
+
+            switch (statusCode) {
+                case SunshineSyncAdapter.LOCATION_STATUS_OK:
+                    preference.setSummary(stringValue);
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN:
+                    preference.setSummary(getString(R.string.pref_location_unknown_description, stringValue));
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_INVALID:
+                    preference.setSummary(getString(R.string.pref_location_error_description, stringValue));
+                    break;
+                default:
+                    //If the server is down we still assume the value is valid
+                    preference.setSummary(stringValue);
+            }
+        }
+        // For other preferences, set the summary to the value's simple string representation.
+        else {
             preference.setSummary(stringValue);
         }
-        return true;
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
