39d8a89e2d7d613843e750e62860a7b519bd1f1d
Safwan Ahmad <safwan.pico@gmail.com>
2017-02-08 18:44:30 +0530

    Add Edit text listener to custom view so that if the text length in the dialog is less than the  minLength specified in the pref_general.xml then ok button is disabled and when length is again equal to or greater than minLength the button is enabled.

--- app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -1,10 +1,18 @@
 package com.example.android.sunshine.app;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * This is our custom view to provide extra/custom attributes on {@Link EditTextPreference}.
@@ -16,9 +24,12 @@ import android.util.Log;
 public class LocationEditTextPreference extends EditTextPreference {
 
     public static final int DEFAULT_MIN_LENGTH = 2;
+    private TextWatcher mEditTextWatcher;
+    private int mMinLength = 0;
 
     public LocationEditTextPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mEditTextWatcher = new EditTextWatcher();
 
         TypedArray array = context.getTheme().obtainStyledAttributes(attrs,
                 R.styleable.LocationEditTextPreference,
@@ -26,12 +37,57 @@ public class LocationEditTextPreference extends EditTextPreference {
                 0);
 
         try {
-            int minLength = array.getInteger(R.styleable.LocationEditTextPreference_minLength, DEFAULT_MIN_LENGTH);
+            mMinLength = array.getInteger(R.styleable.LocationEditTextPreference_minLength, DEFAULT_MIN_LENGTH);
 
             //Log this value
-            Log.d(LocationEditTextPreference.class.getSimpleName(), String.valueOf(minLength));
+            Log.d(LocationEditTextPreference.class.getSimpleName(), String.valueOf(mMinLength));
         } finally {
             array.recycle();
+        }
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+
+        EditText editText = getEditText();
+        editText.addTextChangedListener(mEditTextWatcher);
+    }
+
+    private class EditTextWatcher implements TextWatcher
+    {
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            Log.d(LocationEditTextPreference.class.getSimpleName(), s.toString());
+
+
+            // Check if the length of the input is less than 3 characters long then
+            // we will disable the OK (dialog) button.
+            Button positive = null;
+            Dialog dialog = getDialog();
+
+            //But we must ensure that the associated dialog belongs to alert dialog.
+            if (dialog instanceof AlertDialog) {
+                positive = ((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE);
+            }
+
+            if (positive != null) {
+                if (s.length() < mMinLength) {
+                    positive.setEnabled(false);
+                } else {
+                    positive.setEnabled(true);
+                }
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
         }
     }
 }
